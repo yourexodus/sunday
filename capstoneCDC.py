ca49e973f004808673b6c94cb1091ebff094f95f
Marlainna <marlainna456@gmail.com>
2024-08-11 00:12:12 -0500

    add

--- capstoneCDC.py
+++ capstoneCDC.py
@@ -2,18 +2,14 @@
 import pandas as pd
 import dash
 
-# Creates the interactive dashboard using the dash library
-import pandas as pd
-import dash
-from dash import dash_table
-# from dash_table import DataTable
+from dash_table import DataTable
 from dash.dependencies import Output, Input, State
 import dash_bootstrap_components as dbc
 from dash import html, dcc, dash_table
 from dash import html
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
-from sklearn.ensemble import RandomForestClassifier
+#from sklearn.ensemble import RandomForestClassifier
 from PIL import Image
 
 from prepare import PrepareData
@@ -57,5 +53,4 @@ app.layout = html.Div([
 
 if __name__ == '__main__':
     app.run_server(debug=True )
-
 
